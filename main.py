772c0053891f7c29b8de6696d46657dd65ce8d26
Daniil <ladeishikoff.d@yandex.ru>
2020-12-31 07:47:35 +0000

    Test

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-number = int(input('Сколькл градусов на улице? '))
-number//10
-number%10
-summ
-while weather > 15:
-  meters += 20
-  weather -= 2
-  if weather <= 15:
-    break
-  meters += 10
-  print ('Пройдено метров: ', meters)
+
+bags = int(input('Сколько нужно перенести мешков?'))
+total  = 0
+bags_count = 0
+while bags_count < bags:
+  weigt 
+  print(text)
+  word += 1
 
 
 
@@ -16,6 +13,41 @@ while weather > 15:
 
 
 '''
+
+text = 'Python'
+word = 0
+while word < 5:
+  print(text)
+  word += 1
+
+
+rate_chek = False
+while True:
+  active = int(input('Продолжаем работать?'))
+  if active ==0:
+    print('Приложение закрывается')
+    break
+  rate = int(input('Поставите оценку?'))
+  if rate == 1:
+    rate_chek = True
+print('Работа завершена')
+if rate_chek == True:
+   print('Пользователь, продолжает работать')
+
+
+number = int(input('Сколькл градусов на улице? '))
+summ = 0
+while number != 0:
+  las_numb = number%10
+  print (las_numb)
+  summ += las_numb
+  if las_numb == 5:
+    print ('Обнаружен разрыв')
+    break
+  summ + las_num
+  number//10
+  print ('Сумма:  ', summ)
+
 password = int(input('Введите пароль: '))
 while password != 215:
   print ('Не верный пароль')
